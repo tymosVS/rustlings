47080604fd6088948d238cdce6f3303e0ca3560d
Volodymyr Tymoshenko <timos9Vs@gmail.com>
2022-08-31 15:43:13 +0300

    all exercises

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,7 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -38,13 +37,34 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b) = tuple;
+
+        if [r, g, b].iter().any(|&num| !(0..=255).contains(&num)) {
+            return Err(IntoColorError::IntConversion);
+        } else {
+            Ok(Color {
+                red: r as u8,
+                green: g as u8,
+                blue: b as u8,
+            })
+        }
     }
 }
+
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if arr.iter().any(|&num| !(0..=255).contains(&num)) {
+            return Err(IntoColorError::IntConversion);
+        } else {
+            Ok(Color {
+                red: arr[0] as u8,
+                green: arr[1] as u8,
+                blue: arr[2] as u8,
+            })
+        }
     }
 }
 
@@ -52,6 +72,19 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        if slice.iter().any(|&num| !(0..=255).contains(&num)) {
+            return Err(IntoColorError::IntConversion);
+         } else {
+             Ok(Color {
+                 red: slice[0] as u8,
+                 green: slice[1] as u8,
+                 blue: slice[2] as u8,
+             })
+         }
     }
 }
 
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -13,6 +11,13 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    match num {
+        0 => 1,
+        _ => match (1..=num).into_iter().reduce(|a, b| a * b) {
+            Some(x) => x,
+            _ => 1,
+        }
+    }
 }
 
 #[cfg(test)]
